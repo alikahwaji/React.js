4c7adf61fa22e02397fd8df36bcf2c243f4f61da
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-24 07:11:39 +1200

    Updat file 02_01

--- Ex_Files_React_EssT/Ch02/02_01/finish/dist/index.js
+++ Ex_Files_React_EssT/Ch02/02_01/finish/dist/index.js
@@ -2,12 +2,11 @@ const { createElement } = React
 const { render } = ReactDOM
 
 const title = createElement(
-	'h1',
-	{id: 'title', className: 'header'},
-	'Hello World'
+    'h1', { id: 'title', className: 'header' },
+    'Hello World'
 )
 
 render(
-	title,
-	document.getElementById('react-container')
+    title,
+    document.getElementById('react-container')
 )
