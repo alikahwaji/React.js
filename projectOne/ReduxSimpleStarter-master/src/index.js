e8b053908036bc56e22330e10dbaece21daccc73
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-28 13:02:56 +1200

    Creating a SearchBar component

--- projectOne/ReduxSimpleStarter-master/src/index.js
+++ projectOne/ReduxSimpleStarter-master/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyDt6CGw_cRrHbeN_1T7VK5N_131Nlm8oBA'
+
 const App =  () => {
-    return <div> Hi!</div>
+    return (<div> 
+        <SearchBar />
+    </div>
+    )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
